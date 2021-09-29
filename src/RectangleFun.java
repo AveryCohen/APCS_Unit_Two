5abce1ed93ce78c5a933bd0a278c20e55419e62c
AveryCohen <AveryCohen04@gmail.com>
2021-09-29 10:55:01 -0400

    Finished RectangleFun even though the test says it is wrong when in reality the test is wrong and I am right and awesome and Avery

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -6,6 +6,19 @@ public class RectangleFun {
     public static void main(String[] args) {
         // Create a scanner object to be able to get input from the user
         Scanner scan = new Scanner(System.in);
+        System.out.print("X: ");
+        int x = scan.nextInt();
+        System.out.print("Y: ");
+        int y = scan.nextInt();
+        System.out.print("Width: ");
+        int width = scan.nextInt();
+        System.out.print("Height: ");
+        int height = scan.nextInt();
+        Rectangle r = new Rectangle(x,y,width,height);
+        double perimeter = r.getHeight()*2 + r.getWidth()*2;
+        System.out.println("Perimeter = " + perimeter);
+        r.setLocation((int)((r.getX()) -4),(int)((r.getY()) + 2));
+        System.out.println("New location is (" + r.getX() + ", " + r.getY() + ")");
 
         // Get input for x, y, width, and height. Create four separate variables for these.
 
