7cff75765adf32851eedc49088ce19425284cb4c
AveryCohen <AveryCohen04@gmail.com>
2021-10-12 10:57:21 -0400

    Finished the first 3 math methods

--- src/Distance.java
+++ src/Distance.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class Distance {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((Math.pow(x2-x1,2) + Math.pow(y2-y1,2)));
+    }
+    public static void main(String[] args) {
+        // Scanner object to get input
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the x coordinate of the first point: ");
+        int x1 = scan.nextInt();
+        System.out.print("Enter the y coordinate of the first point: ");
+        int y1 = scan.nextInt();
+        System.out.print("Enter the x coordinate of the second point: ");
+        int x2 = scan.nextInt();
+        System.out.print("Enter the y coordinate of the second point: ");
+        int y2 = scan.nextInt();
+        double dis = distance(x1, y1, x2, y2);
+        System.out.println("The distance between (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is " + dis);
+    }
+
 
     // create the distance method below. See the instructions for the signature of the method.
 
--- src/FindThePower.java
+++ src/FindThePower.java
@@ -1,6 +1,21 @@
-
+import java.util.Scanner;
 
 public class FindThePower {
+    public static int findPower(double num1, double pow1) {
+
+        return (int)Math.pow(num1, pow1);
+    }
+    public static void main(String[] args) {
+        // Scanner object to get input
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the base: ");
+        double num1 = scan.nextDouble();
+        System.out.print("Enter the exponent: ");
+        double pow1 = scan.nextDouble();
+        int product = findPower(num1, pow1);
+        System.out.println((int)num1 + "^" + (int)pow1 + " = " + product);
+    }
+
 
     // create a main method below and follow the given requirements carefully.
     // Remember to create a Scanner object in order to get input from the user.
